1f7d88a6a53b76d164715d198e3898d857aaaf9a
aseleznev <aa.seleznev@gmail.com>
2020-08-06 10:51:57 +0500

    change config params

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const prod = process.env.NODE_ENV === 'production';
 module.exports = {
     port: process.env.PORT || 3000,
     staticRoute: '/uploads', // The URL portion
-    staticPath: '../gazeta-upload', // The local path on disk
+    staticPath: 'gazeta-upload', // The local path on disk
     distDir: 'dist',
     host: prod ? '//gazeta.gn.com.ru' : dev ? '//newspaper-dev.gp-ggr.ru' : '//localhost:3000',
     backendUrl: prod
@@ -12,12 +12,15 @@ module.exports = {
         : dev
         ? 'https://newspaper-dev.gp-ggr.ru/api'
         : 'http://localhost:3001/api',
-    tinyMceBaseUrl: prod || dev ? '/admin/tinymce-assets' : '/tinymce-assets',
+    tinyMceBaseUrl: prod || dev ? '/tinymce-assets' : '/tinymce-assets',
     dbconnection: prod
-        ? 'postgres://node:node@localhost/gazeta_cms'
+        ? 'postgres://cms:cms@localhost/gazeta_cms'
         : dev
         ? 'postgres://cms:cms@localhost/gazeta_cms'
         : 'postgres://localhost/gazeta_cms3',
     apiKey: 'f44d32aa-5a77-4363-b190-ea1f8d2a658d',
-    cookieSecret: 'c68ab997285c70f0f9b59281b34fbc2c717de2597fb0b1d2c4049d56aa8b6050'
+    cookieSecret: 'c68ab997285c70f0f9b59281b34fbc2c717de2597fb0b1d2c4049d56aa8b6050',
+    ssl: prod,
+    cert: '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+    key: '/etc/ssl/private/ssl-cert-snakeoil.key'
 };
